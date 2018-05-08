e0978d67321007e53db3a87afd34b2260d430c0f
higginss <shigger@gmail.com>
2018-05-08 14:21:47 +0100

    First commit to git for the new project (all files added except binaries).

---
+++ src/main/java/com/higginss/vinylbox/VinylboxApplication.java
@@ -0,0 +1,12 @@
+package com.higginss.vinylbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinylboxApplication.class, args);
+    }
+}
---
+++ src/test/java/com/higginss/vinylbox/VinylboxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.higginss.vinylbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VinylboxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

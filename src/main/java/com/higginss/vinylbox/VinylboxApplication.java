8b359c4ab0372cc0c586b7002f7f381e6065fe58
higginss <shigger@gmail.com>
2018-05-09 12:05:40 +0100

    Test change.

--- src/main/java/com/higginss/vinylbox/VinylboxApplication.java
+++ src/main/java/com/higginss/vinylbox/VinylboxApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VinylboxApplication {
 
     public static void main(String[] args) {
+        System.out.println("Running main...");
         SpringApplication.run(VinylboxApplication.class, args);
     }
 }
